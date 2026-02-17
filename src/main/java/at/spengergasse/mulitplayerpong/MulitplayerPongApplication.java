ff384de901b8c7d85796bcbe9bc45e9255395627
Felix Huber <felilou@aon.at>
2026-02-17 17:50:58 +0100

    inital commit

---
+++ src/main/java/at/spengergasse/mulitplayerpong/MulitplayerPongApplication.java
@@ -0,0 +1,13 @@
+package at.spengergasse.mulitplayerpong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MulitplayerPongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MulitplayerPongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/spengergasse/mulitplayerpong/MulitplayerPongApplicationTests.java
@@ -0,0 +1,13 @@
+package at.spengergasse.mulitplayerpong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MulitplayerPongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
